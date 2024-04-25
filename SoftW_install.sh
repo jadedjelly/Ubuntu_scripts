8fcba3f088785cae28d672fa7c5183617560b02a
jadedjelly <kellyjd83@gmail.com>
2024-04-25 09:15:44 +0100

    created script for int installation, not complete

---
+++ SoftW_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 
+
+
+sudo apt-get install git-all
+
+
+sudo snap install spotify
+sudo snap install discord
+sudo snap install thunderbird
+sudo snap install intellij-idea-community --classic
+
+source 
